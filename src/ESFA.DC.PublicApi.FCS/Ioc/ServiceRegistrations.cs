f67587215b6da25d47c960c9cdb256e28044c946
Salman Baig <salman.sfa@outlook.com>
2020-04-08 16:27:11 +0100

    changes to use standard base startup

--- src/ESFA.DC.PublicApi.FCS/Ioc/ServiceRegistrations.cs
+++ src/ESFA.DC.PublicApi.FCS/Ioc/ServiceRegistrations.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using Autofac;
-using ESFA.DC.Api.Common.Identity.EF;
-using ESFA.DC.Api.Common.Identity.EF.Interfaces;
-using ESFA.DC.Api.Common.Identity.Services;
+using ESFA.DC.Api.Common.Settings;
 using ESFA.DC.PublicApi.FCS.Services;
 using ESFA.DC.PublicApi.FCS.Settings;
 using ESFA.DC.Summarisation.Model;
 using ESFA.DC.Summarisation.Model.Interface;
-using ESFA.DC.WebApi.External.Settings;
 using Microsoft.EntityFrameworkCore;
 
 namespace ESFA.DC.PublicApi.FCS.Ioc
@@ -18,31 +15,16 @@ namespace ESFA.DC.PublicApi.FCS.Ioc
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<SummarisedActualsRepository>().As<ISummarisedActualsRepository>();
-            builder.RegisterType<IdentityService>().As<IIdentityService>();
 
             // Db contexts
             builder.RegisterType<SummarisationContext>().As<ISummarisationContext>().ExternallyOwned();
-            builder.RegisterType<ApiIdentityContext>().As<IApiIdentityContext>().ExternallyOwned();
 
-            builder.Register(context =>
-                {
-                    var connectionStrings = context.Resolve<ConnectionStrings>();
-                    var optionsBuilder = new DbContextOptionsBuilder<ApiIdentityContext>();
-                    optionsBuilder.UseSqlServer(
-                        connectionStrings.WebApiExternalIdentity,
-                        options => options.EnableRetryOnFailure(3, TimeSpan.FromSeconds(3), new List<int>()));
-
-                    return optionsBuilder.Options;
-                })
-                .As<DbContextOptions<ApiIdentityContext>>()
-                .SingleInstance();
-
-            builder.Register(context =>
+           builder.Register(context =>
                 {
                     var connectionStrings = context.Resolve<ConnectionStrings>();
                     var optionsBuilder = new DbContextOptionsBuilder<SummarisationContext>();
                     optionsBuilder.UseSqlServer(
-                        connectionStrings.SummarisedActualsDatastore,
+                        connectionStrings.KeyValues["SummarisedActualsDatastore"],
                         options => options.EnableRetryOnFailure(3, TimeSpan.FromSeconds(3), new List<int>()));
 
                     return optionsBuilder.Options;
