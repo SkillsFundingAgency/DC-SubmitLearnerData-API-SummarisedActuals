1344a6b22ee2ecc8dc50879c01e0ef9ddc881994
Salman Baig <salman.sfa@outlook.com>
2020-05-19 10:18:19 +0100

    Updated to use new async method

--- src/ESFA.DC.PublicApi.FCS.Services/SummarisedActualsRepository.cs
+++ src/ESFA.DC.PublicApi.FCS.Services/SummarisedActualsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using ESFA.DC.Api.Common.Paging.Interfaces;
 using ESFA.DC.Api.Common.Paging.Pagination;
@@ -26,11 +27,9 @@ namespace ESFA.DC.PublicApi.FCS.Services
 
             using (var context = _summarisationFactory())
             {
-                var collectionReturnId = (await context.CollectionReturns.FirstOrDefaultAsync(x => x.CollectionType == collectionType &&
-                                                                                                   x.CollectionReturnCode == collectionReturnCode))?.Id;
-
                 var data = context.SummarisedActuals
-                    .Where(x => x.CollectionReturnId == collectionReturnId)
+                    .Where(x => x.CollectionReturn.CollectionReturnCode == collectionReturnCode &&
+                                x.CollectionReturn.CollectionType == collectionType)
                     .Select(x => new SummarisedActualDto
                     {
                         CollectionReturnCode = collectionReturnCode,
@@ -48,14 +47,13 @@ namespace ESFA.DC.PublicApi.FCS.Services
                     })
                     .OrderBy(x => x.Id);
 
-                result = await PaginatedResultFactory<SummarisedActualDto>.CreatePaginatedResult(data, pageSize, pageNumber);
+                result = await PaginatedResultFactory<SummarisedActualDto>.CreatePaginatedResultAsync(data, pageSize, pageNumber, CancellationToken.None);
             }
 
             return result;
         }
 
-        public async Task<IPaginatedResult<CollectionReturnDto>> GetClosedCollectionEvents(
-            DateTime? closedCollectionsSince = null, int pageSize = 0, int pageNumber = 0)
+        public async Task<IPaginatedResult<CollectionReturnDto>> GetClosedCollectionEvents(DateTime? closedCollectionsSince = null, int pageSize = 0, int pageNumber = 0)
         {
             IPaginatedResult<CollectionReturnDto> result;
 
@@ -78,7 +76,7 @@ namespace ESFA.DC.PublicApi.FCS.Services
                         TotalItems = grp.Count()
                     });
 
-                result = await PaginatedResultFactory<CollectionReturnDto>.CreatePaginatedResult(data, pageSize, pageNumber);
+                result = await PaginatedResultFactory<CollectionReturnDto>.CreatePaginatedResultAsync(data, pageSize, pageNumber, CancellationToken.None);
             }
 
             return result;
